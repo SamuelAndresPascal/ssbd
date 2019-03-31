dda0fc1024b03ac64039b75d239e1f2660263b8c
Samuel Andrés <samuelandres@yandex.com>
2019-03-31 23:17:19 +0200

    feat(ssbd): update ellipsoid schema + data
    use table-context ids

--- ssbd_data.sql
+++ ssbd_data.sql
@@ -33,65 +33,95 @@ insert into ssbd_body values (516, 'Metis', 'direct', null, null);
 insert into ssbd_body values (699, 'Saturn', 'direct', null, null);
 insert into ssbd_body values (799, 'Uranus', 'indirect', null, null);
 insert into ssbd_body values (899, 'Neptune', 'direct', null, null);
+insert into ssbd_body values (999, 'Pluto', 'indirect', null, null);
 
 
-insert into ssbd_ellipsoid values (199, 502000199, 'Mercury 2000 IAU', 2439700.00, 9001, null, null, null, null, null, 'Report of the IAU/IAG working group on cartographic coordinates and rotational elements of the planets and satellites: 2000');
-insert into ssbd_ellipsoid values (299, 502000299, 'Venus 2000 IAU', 6051800.00, 9001, null, null, null, null, null, 'Report of the IAU/IAG working group on cartographic coordinates and rotational elements of the planets and satellites: 2000');
-insert into ssbd_ellipsoid values (399, 502000399, 'Spherical Earth 2000 IAU', 6371000.00, 9001, null, null, null, null, null, 'Report of the IAU/IAG working group on cartographic coordinates and rotational elements of the planets and satellites: 2000');
-insert into ssbd_ellipsoid values (399, 512000399, 'Ellipsoidal Earth 2000 IAU', 6378140.00, 9001, null, 6356750.00, null, null, null, 'Report of the IAU/IAG working group on cartographic coordinates and rotational elements of the planets and satellites: 2000');
-insert into ssbd_ellipsoid values (499, 502000499, 'Spherical Mars 2000 IAU', 3389500.00, 9001, null, null, null, null, null, 'Report of the IAU/IAG working group on cartographic coordinates and rotational elements of the planets and satellites: 2000');
-insert into ssbd_ellipsoid values (499, 512000499, 'Ellipsoidal Mars 2000 IAU', 3396190.00, 9001, null, 3376200.00, null, null, null, 'Report of the IAU/IAG working group on cartographic coordinates and rotational elements of the planets and satellites: 2000');
-insert into ssbd_ellipsoid values (499, 522000499, 'Quadriaxial Mars 2000 IAU', 3396190.00, 9001, null, 3373190.00, null, 3379210.00, 'This ellipsoid is considered quadriaxial even if subplanetary and along orbit equatorial axis are equal.', 'Report of the IAU/IAG working group on cartographic coordinates and rotational elements of the planets and satellites: 2000');
-insert into ssbd_ellipsoid values (599, 502000599, 'Spherical Jupiter 2000 IAU', 69911000.00, 9001, null, null, null, null, null, 'Report of the IAU/IAG working group on cartographic coordinates and rotational elements of the planets and satellites: 2000');
-insert into ssbd_ellipsoid values (599, 512000599, 'Ellipsoidal Jupiter 2000 IAU', 71492000.00, 9001, null, 66854000.00, null, null, null, 'Report of the IAU/IAG working group on cartographic coordinates and rotational elements of the planets and satellites: 2000');
-insert into ssbd_ellipsoid values (301, 502000301, 'Spherical Moon 2000 IAU', 1737400.00, 9001, null, null, null, null, null, 'Report of the IAU/IAG working group on cartographic coordinates and rotational elements of the planets and satellites: 2000');
-insert into ssbd_ellipsoid values (401, 502000401, 'Spherical Phobos 2000 IAU', 11100.00, 9001, null, null, null, null, null, 'Report of the IAU/IAG working group on cartographic coordinates and rotational elements of the planets and satellites: 2000');
-insert into ssbd_ellipsoid values (401, 512000401, 'Triaxial Phobos 2000 IAU', 13400.00, 9001, null, 9200.00, 11200.00, null, null, 'Report of the IAU/IAG working group on cartographic coordinates and rotational elements of the planets and satellites: 2000');
-insert into ssbd_ellipsoid values (402, 502000402, 'Spherical Deimos 2000 IAU', 6200.00, 9001, null, null, null, null, null, 'Report of the IAU/IAG working group on cartographic coordinates and rotational elements of the planets and satellites: 2000');
-insert into ssbd_ellipsoid values (402, 512000402, 'Triaxial Deimos 2000 IAU', 7500.00, 9001, null, 5200.00, 6100.00, null, null, 'Report of the IAU/IAG working group on cartographic coordinates and rotational elements of the planets and satellites: 2000');
+insert into ssbd_ellipsoid values (199, 1, 'Mercury 2000 IAU', 2439700.00, 1000., 9001, null, null, null,
+ null, null, null, null, null, 'Report of the IAU/IAG working group on cartographic coordinates and rotational elements of the planets and satellites: 2000');
+insert into ssbd_ellipsoid values (299, 2, 'Venus 2000 IAU', 6051800.00, 1000., 9001, null, null, null,
+ null, null, null, null, null, 'Report of the IAU/IAG working group on cartographic coordinates and rotational elements of the planets and satellites: 2000');
+insert into ssbd_ellipsoid values (399, 3, 'Spherical Earth 2000 IAU', 6371000.00, 10., 9001, null, null, null,
+ null, null, null, null, null, 'Report of the IAU/IAG working group on cartographic coordinates and rotational elements of the planets and satellites: 2000');
+insert into ssbd_ellipsoid values (399, 4, 'Ellipsoidal Earth 2000 IAU', 6378140.00, 10., 9001, null, 6356750.00, 10.,
+ null, null, null, null, null, 'Report of the IAU/IAG working group on cartographic coordinates and rotational elements of the planets and satellites: 2000');
+insert into ssbd_ellipsoid values (499, 5, 'Spherical Mars 2000 IAU', 3389500.00, 200., 9001, null, null, null,
+ null, null, null, null, null, 'Report of the IAU/IAG working group on cartographic coordinates and rotational elements of the planets and satellites: 2000');
+insert into ssbd_ellipsoid values (499, 6, 'Ellipsoidal Mars 2000 IAU', 3396190.00, 100., 9001, null, 3376200.00, 100.,
+ null, null, null, null, null, 'Report of the IAU/IAG working group on cartographic coordinates and rotational elements of the planets and satellites: 2000');
+insert into ssbd_ellipsoid values (499, 7, 'Quadriaxial Mars 2000 IAU', 3396190.00, 100., 9001, null, 3373190.00, 100.,
+ null, null, 3379210.00, 100., 'This ellipsoid is considered quadriaxial even if subplanetary and along orbit equatorial axis are equal.', 'Report of the IAU/IAG working group on cartographic coordinates and rotational elements of the planets and satellites: 2000');
+insert into ssbd_ellipsoid values (599, 8, 'Spherical Jupiter 2000 IAU', 69911000.00, 6000., 9001, null, null, null,
+ null, null, null, null, null, 'Report of the IAU/IAG working group on cartographic coordinates and rotational elements of the planets and satellites: 2000');
+insert into ssbd_ellipsoid values (599, 9, 'Ellipsoidal Jupiter 2000 IAU', 71492000.00,  4000., 9001, null, 66854000.00, 10000.,
+ null, null, null, null, null, 'Report of the IAU/IAG working group on cartographic coordinates and rotational elements of the planets and satellites: 2000');
+insert into ssbd_ellipsoid values (699, 10, 'Spherical Saturn 2000 IAU', 58232000.00, 6000., 9001, null, null, null,
+ null, null, null, null, null, 'Report of the IAU/IAG working group on cartographic coordinates and rotational elements of the planets and satellites: 2000');
+insert into ssbd_ellipsoid values (699, 11, 'Ellipsoidal Saturn 2000 IAU', 60268000.00,  4000., 9001, null, 54364000.00, 10000.,
+ null, null, null, null, null, 'Report of the IAU/IAG working group on cartographic coordinates and rotational elements of the planets and satellites: 2000');
+insert into ssbd_ellipsoid values (799, 12, 'Spherical Uranus 2000 IAU', 25362000.00, 7000., 9001, null, null, null,
+ null, null, null, null, null, 'Report of the IAU/IAG working group on cartographic coordinates and rotational elements of the planets and satellites: 2000');
+insert into ssbd_ellipsoid values (799, 13, 'Ellipsoidal Uranus 2000 IAU', 25559000.00,  4000., 9001, null, 24973000.00, 20000.,
+ null, null, null, null, null, 'Report of the IAU/IAG working group on cartographic coordinates and rotational elements of the planets and satellites: 2000');
+insert into ssbd_ellipsoid values (899, 14, 'Spherical Neptune 2000 IAU', 24622000.00, 19000., 9001, null, null, null,
+ null, null, null, null, null, 'Report of the IAU/IAG working group on cartographic coordinates and rotational elements of the planets and satellites: 2000');
+insert into ssbd_ellipsoid values (899, 15, 'Ellipsoidal Neptune 2000 IAU', 24764000.00,  15000., 9001, null, 24341000.00, 30000.,
+ null, null, null, null, null, 'Report of the IAU/IAG working group on cartographic coordinates and rotational elements of the planets and satellites: 2000');
+insert into ssbd_ellipsoid values (999, 16, 'Spherical Pluto 2000 IAU', 1195000.00,  5000., 9001, null, null, null,
+ null, null, null, null, null, 'Report of the IAU/IAG working group on cartographic coordinates and rotational elements of the planets and satellites: 2000');
+
+insert into ssbd_ellipsoid values (301, 17, 'Spherical Moon 2000 IAU', 1737400.00, 1000., 9001, null, null, null,
+ null, null, null, null, null, 'Report of the IAU/IAG working group on cartographic coordinates and rotational elements of the planets and satellites: 2000');
+insert into ssbd_ellipsoid values (401, 18, 'Spherical Phobos 2000 IAU', 11100.00, 150., 9001, null, null, null,
+ null, null, null, null, null, 'Report of the IAU/IAG working group on cartographic coordinates and rotational elements of the planets and satellites: 2000');
+insert into ssbd_ellipsoid values (401, 19, 'Triaxial Phobos 2000 IAU', 13400.00, null, 9001, null, 9200.00, null,
+ 11200.00, null, null, null, null, 'Report of the IAU/IAG working group on cartographic coordinates and rotational elements of the planets and satellites: 2000');
+insert into ssbd_ellipsoid values (402, 20, 'Spherical Deimos 2000 IAU', 6200.00, 180., 9001, null, null, null,
+ null, null, null, null, null, 'Report of the IAU/IAG working group on cartographic coordinates and rotational elements of the planets and satellites: 2000');
+insert into ssbd_ellipsoid values (402, 21, 'Triaxial Deimos 2000 IAU', 7500.00, null, 9001, null, 5200.00, null,
+ 6100.00, null, null, null, null, 'Report of the IAU/IAG working group on cartographic coordinates and rotational elements of the planets and satellites: 2000');
 
 
-insert into ssbd_primemeridiansystem values (160199, 'Mercury crust system', 199, null, null);
-insert into ssbd_primemeridiansystem values (160299, 'Venus crust system', 299, null, null);
-insert into ssbd_primemeridiansystem values (160499, 'Mars crust system', 499, null, null);
-insert into ssbd_primemeridiansystem values (160599, 'System I Jupiter', 599, 'Refers to the mean atmospheric equatorial rotation.', null);
-insert into ssbd_primemeridiansystem values (161599, 'System II Jupiter', 599, 'Refers to the mean atmospheric rotation north of the south component of the north equatorial belt, and south of the north component of the south equatorial belt.', null);
-insert into ssbd_primemeridiansystem values (162599, 'System III Jupiter', 599, 'Refers to the magnetic field rotation.', null);
-insert into ssbd_primemeridiansystem values (160699, 'System I Saturn', 699, 'Refers to the mean atmospheric equatorial rotation.', null);
-insert into ssbd_primemeridiansystem values (161699, 'System II Saturn', 699, 'Refers to the mean atmospheric rotation north of the south component of the north equatorial belt, and south of the north component of the south equatorial belt.', null);
-insert into ssbd_primemeridiansystem values (162699, 'System III Saturn', 699, 'Refers to the magnetic field rotation.', null);
-insert into ssbd_primemeridiansystem values (160799, 'System I Uranus', 799, 'Refers to the mean atmospheric equatorial rotation.', null);
-insert into ssbd_primemeridiansystem values (161799, 'System II Uranus', 799, 'Refers to the mean atmospheric rotation north of the south component of the north equatorial belt, and south of the north component of the south equatorial belt.', null);
-insert into ssbd_primemeridiansystem values (162799, 'System III Uranus', 799, 'Refers to the magnetic field rotation.', null);
-insert into ssbd_primemeridiansystem values (161899, 'System II Neptune', 799, 'Refers to the rotation of optically observed features in the Neptunian atmosphere.', null);
+insert into ssbd_primemeridiansystem values (1, 'Mercury crust system', 199, null, null);
+insert into ssbd_primemeridiansystem values (2, 'Venus crust system', 299, null, null);
+insert into ssbd_primemeridiansystem values (3, 'Mars crust system', 499, null, null);
+insert into ssbd_primemeridiansystem values (4, 'System I Jupiter', 599, 'Refers to the mean atmospheric equatorial rotation.', null);
+insert into ssbd_primemeridiansystem values (5, 'System II Jupiter', 599, 'Refers to the mean atmospheric rotation north of the south component of the north equatorial belt, and south of the north component of the south equatorial belt.', null);
+insert into ssbd_primemeridiansystem values (6, 'System III Jupiter', 599, 'Refers to the magnetic field rotation.', null);
+insert into ssbd_primemeridiansystem values (7, 'System I Saturn', 699, 'Refers to the mean atmospheric equatorial rotation.', null);
+insert into ssbd_primemeridiansystem values (8, 'System II Saturn', 699, 'Refers to the mean atmospheric rotation north of the south component of the north equatorial belt, and south of the north component of the south equatorial belt.', null);
+insert into ssbd_primemeridiansystem values (9, 'System III Saturn', 699, 'Refers to the magnetic field rotation.', null);
+insert into ssbd_primemeridiansystem values (10, 'System I Uranus', 799, 'Refers to the mean atmospheric equatorial rotation.', null);
+insert into ssbd_primemeridiansystem values (11, 'System II Uranus', 799, 'Refers to the mean atmospheric rotation north of the south component of the north equatorial belt, and south of the north component of the south equatorial belt.', null);
+insert into ssbd_primemeridiansystem values (12, 'System III Uranus', 799, 'Refers to the magnetic field rotation.', null);
+insert into ssbd_primemeridiansystem values (13, 'System II Neptune', 799, 'Refers to the rotation of optically observed features in the Neptunian atmosphere.', null);
 
 
-insert into ssbd_primemeridian values (170160199, 'Hun Kal Mercury Meridian', 0., 'prograde', 20., 20., 'prograde', 9102, 160199, null, null);
-insert into ssbd_primemeridian values (170160299, 'Ariadne Venus Meridian', 0., 'prograde', .0, .0, 'prograde', 9102, 160299, null, null);
-insert into ssbd_primemeridian values (170160499, 'Airy-0 Mars Meridian', 0., 'prograde', .0, .0, 'prograde', 9102, 160499, null, null);
-insert into ssbd_primemeridian values (170160599, 'System I Jupiter Meridian', 0., 'prograde', .0, .0, 'prograde', 9102, 160599, null, null);
-insert into ssbd_primemeridian values (170161599, 'System II Jupiter Meridian', 0., 'prograde', .0, .0, 'prograde', 9102, 161599, null, null);
-insert into ssbd_primemeridian values (170162599, 'System III Jupiter Meridian', 0., 'prograde', .0, .0, 'prograde', 9102, 162599, null, null);
-insert into ssbd_primemeridian values (170160699, 'System I Saturn Meridian', 0., 'prograde', .0, .0, 'prograde', 9102, 160699, null, null);
-insert into ssbd_primemeridian values (170161699, 'System II Saturn Meridian', 0., 'prograde', .0, .0, 'prograde', 9102, 161699, null, null);
-insert into ssbd_primemeridian values (170162699, 'System III Saturn Meridian', 0., 'prograde', .0, .0, 'prograde', 9102, 162699, null, null);
-insert into ssbd_primemeridian values (170160799, 'System I Uranus Meridian', 0., 'prograde', .0, .0, 'prograde', 9102, 160799, null, null);
-insert into ssbd_primemeridian values (170161799, 'System II Uranus Meridian', 0., 'prograde', .0, .0, 'prograde', 9102, 161799, null, null);
-insert into ssbd_primemeridian values (170162799, 'System III Uranus Meridian', 0., 'prograde', .0, .0, 'prograde', 9102, 162799, null, null);
-insert into ssbd_primemeridian values (170161899, 'System II Neptune Meridian', 0., 'prograde', .0, .0, 'prograde', 9102, 161899, null, null);
+insert into ssbd_primemeridian values (1, 'Hun Kal Mercury Meridian', 0., 'prograde', 20., 20., 'prograde', 9102, 1, null, null);
+insert into ssbd_primemeridian values (2, 'Ariadne Venus Meridian', 0., 'prograde', .0, .0, 'prograde', 9102, 2, null, null);
+insert into ssbd_primemeridian values (3, 'Airy-0 Mars Meridian', 0., 'prograde', .0, .0, 'prograde', 9102, 3, null, null);
+insert into ssbd_primemeridian values (4, 'System I Jupiter Meridian', 0., 'prograde', .0, .0, 'prograde', 9102, 4, null, null);
+insert into ssbd_primemeridian values (5, 'System II Jupiter Meridian', 0., 'prograde', .0, .0, 'prograde', 9102, 5, null, null);
+insert into ssbd_primemeridian values (6, 'System III Jupiter Meridian', 0., 'prograde', .0, .0, 'prograde', 9102, 6, null, null);
+insert into ssbd_primemeridian values (7, 'System I Saturn Meridian', 0., 'prograde', .0, .0, 'prograde', 9102, 7, null, null);
+insert into ssbd_primemeridian values (8, 'System II Saturn Meridian', 0., 'prograde', .0, .0, 'prograde', 9102, 8, null, null);
+insert into ssbd_primemeridian values (9, 'System III Saturn Meridian', 0., 'prograde', .0, .0, 'prograde', 9102, 9, null, null);
+insert into ssbd_primemeridian values (10, 'System I Uranus Meridian', 0., 'prograde', .0, .0, 'prograde', 9102, 10, null, null);
+insert into ssbd_primemeridian values (11, 'System II Uranus Meridian', 0., 'prograde', .0, .0, 'prograde', 9102, 11, null, null);
+insert into ssbd_primemeridian values (12, 'System III Uranus Meridian', 0., 'prograde', .0, .0, 'prograde', 9102, 12, null, null);
+insert into ssbd_primemeridian values (13, 'System II Neptune Meridian', 0., 'prograde', .0, .0, 'prograde', 9102, 13, null, null);
 
-insert into ssbd_range values (300001, -180., 180., 'Typically used for earth longitudes.', null);
-insert into ssbd_range values (300002, 0., 360., 'Typically used for planetary longitudes.', null);
-
-
-insert into ssbd_datum values (42000199, 'Mercury 2000', 'geodetic', null, null, 502000199, 170160199, 'Planetology.', null, null);
+insert into ssbd_range values (1, -180., 180., 'Typically used for earth longitudes.', null);
+insert into ssbd_range values (2, 0., 360., 'Typically used for planetary longitudes.', null);
 
 
-insert into ssbd_coordinatereferencesystem values (19900, 'Mercury 2000 planetocentric 3D', 'planetocentric 3D', 6404, 42000199, null, null, null, null, null, null);
-insert into ssbd_coordinatereferencesystem values (19901, 'Mercury 2000 planetographic 3D', 'planetographic 3D', 6423, 42000199, null, null, null, null, null, null);
+insert into ssbd_datum values (1, 'Mercury 2000', 'geodetic', null, null, 1, 1, 'Planetology.', null, null);
 
 
-insert into ssbd_coordinatereferencesystemrange values (19900, 300002, 61, null, null);
-insert into ssbd_coordinatereferencesystemrange values (19901, 300002, 109, null, null);
+insert into ssbd_coordinatereferencesystem values (1, 'Mercury 2000 planetocentric 3D', 'planetocentric 3D', 6404, 1, null, null, null, null, null, null);
+insert into ssbd_coordinatereferencesystem values (2, 'Mercury 2000 planetographic 3D', 'planetographic 3D', 6423, 1, null, null, null, null, null, null);
+
+
+insert into ssbd_coordinatereferencesystemrange values (1, 2, 61, null, null);
+insert into ssbd_coordinatereferencesystemrange values (2, 2, 109, null, null);
 
 commit;
